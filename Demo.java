8695a289ebb72109ac86e8675bd45132a18dce5b
Tulasikrishna <tulasi@gmail.com>
2025-10-08 12:56:12 +0530

    comments added

--- Demo.java
+++ Demo.java
@@ -4,5 +4,8 @@ Public static void main(String args[]){
   System.out.println("This is Java Program");
   String str="hello";
   int a=10;
+System.out.println("Less priority file");
+//this is the comment lline
+/*This is used foe multi line comment this demo class s used for demo purpose */
 }
 }
